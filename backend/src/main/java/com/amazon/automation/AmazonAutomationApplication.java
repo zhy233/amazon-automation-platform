55909a84a55199b760e004d8f9fd0aa44ede0265
zhy233 <38058333+zhy233@users.noreply.github.com>
2025-07-08 22:56:10 +0800

    添加Spring Boot应用程序主类

---
+++ backend/src/main/java/com/amazon/automation/AmazonAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonAutomationApplication.class, args);
+    }
+
+}
